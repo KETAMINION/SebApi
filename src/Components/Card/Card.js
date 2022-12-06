72c5430d91aab4eb962b73b797e718bad2b1d45f
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-12-06 00:32:44 +0000

    setting up alt image

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card(props){
     return [<div className="card-component">
-        <img src={props.src} height="100px"></img>
+        <img src={props.src} height="100px" alt="kwadrat"></img>
         <h1>Name: {props.h1}</h1>
         {/* <p>Type: {props.p}</p> */}
     </div>]
